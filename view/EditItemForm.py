a9146a4594c3c4121f9a05930b389dcc40824bfd
Ruslan Hadyniak <Ruslan.Hadyniak@gmail.com>
2012-11-15 10:27:16 +0200

    view 0.43 (pep8proof)

--- view/EditItemForm.py
+++ view/EditItemForm.py
@@ -1,53 +1,60 @@
-# coding: utf-8 
+# coding: utf-8
 
 import TkForm as tkf
 import Tkinter as Tk
 from Tkinter import StringVar
 
+
 class EditItemForm(tkf.TkForm):
-    def __init__(self,headers,item=None):        
+    def __init__(self, headers, item=None):
         tkf.TkForm.__init__(self)
         searchFrame = Tk.Frame(self)
-        searchFrame.pack(side='top',fill='both', expand=True)
+        searchFrame.pack(side='top', fill='both', expand=True)
         searchFrameLeft = Tk.Frame(searchFrame)
-        searchFrameLeft.pack(side='left',fill='x', expand=True)
+        searchFrameLeft.pack(side='left', fill='x', expand=True)
         searchFrameRight = Tk.Frame(searchFrame)
-        searchFrameRight.pack(side='left',fill='both', expand=True)
-        self.editInputs = {}        
-        for i in range(len(headers)) :    
-            label = Tk.Label(searchFrameLeft,text=headers[i])
-            label.pack(side='top',fill='x', expand=True)          
+        searchFrameRight.pack(side='left', fill='both', expand=True)
+        self.editInputs = {}
+        for i in range(len(headers)):
+            label = Tk.Label(searchFrameLeft, text=headers[i])
+            label.pack(side='top', fill='x', expand=True)
             inputText = StringVar()
-            if item != None:
+            if item is not None:
                 inputText.set(item[i])
-            self.editInputs[i] = Tk.Entry(searchFrameRight,textvariable=inputText)
-            self.editInputs[i].pack(side='top',fill='both', expand=True)
+            self.editInputs[i] = Tk.Entry(searchFrameRight,
+                                          textvariable=inputText)
+            self.editInputs[i].pack(side='top', fill='both', expand=True)
         self.bottomFrame = Tk.Frame(self)
-        self.bottomFrame.pack(side='bottom',fill='both', expand=True)        
-    def addButton(self,name,action):
-        btn = Tk.Button(self.bottomFrame,text=name, width=10)
+        self.bottomFrame.pack(side='bottom', fill='both', expand=True)
+
+    def addButton(self, name, action):
+        btn = Tk.Button(self.bottomFrame, text=name, width=10)
         btn.config(command=action)
-        btn.pack(side='left')        
-    def enableInput(self,ind):
+        btn.pack(side='left')
+
+    def enableInput(self, ind):
         self.editInputs[ind].config(state='normal')
-    def disableInput(self,ind):
+
+    def disableInput(self, ind):
         self.editInputs[ind].config(state='disabled')
+
     def clearInputs(self):
-        for i in range(len(self.editInputs)): 
+        for i in range(len(self.editInputs)):
             self.editInputs[i].config(textvariable=StringVar())
+
     def getRecord(self):
         sr = {}
-        for i in range(len(self.editInputs)): 
-            value = self.editInputs[i].get()             
+        for i in range(len(self.editInputs)):
+            value = self.editInputs[i].get()
             sr[i] = value
-        return sr    
-     
-def main():    
-    headers = ('test1','test2','test3')
-    item = ('i1','i2','i3')
-    eif = EditItemForm(headers,item)
+        return sr
+
+
+def main():
+    headers = ('test1', 'test2', 'test3')
+    item = ('i1', 'i2', 'i3')
+    eif = EditItemForm(headers, item)
     eif.disableInput(0)
-    eif.clearInputs()
     eif.title('EditItemForm demo')
     eif.addButton('TestBtn1', None)
     eif.show()
--- view/TkForm.py
+++ view/TkForm.py
@@ -5,6 +5,7 @@ Created on Nov 15, 2012
 '''
 import Tkinter as Tk
 
+
 class TkForm(Tk.Toplevel):
     '''
     Parent form based on Tk.TopLevel
@@ -15,11 +16,13 @@ class TkForm(Tk.Toplevel):
         '''
         self.root = Tk.Tk()
         self.root.withdraw()
-        Tk.Toplevel.__init__(self, self.root) 
-        self.protocol('WM_DELETE_WINDOW', self.hide)       
-    def show(self):    
-        '''Opens this window'''    
+        Tk.Toplevel.__init__(self, self.root)
+        self.protocol('WM_DELETE_WINDOW', self.hide)
+
+    def show(self):
+        '''Opens this window'''
         self.root.mainloop()
+
     def hide(self):
         '''Closes this window'''
-        self.master.destroy()    
+        self.master.destroy()
